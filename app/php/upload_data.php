d1d9cd45077797d76b31264197117d92474e699e
Brendan Giberson <bgibers@clemson.edu>
2018-09-11 21:29:34 -0400

    Upload :: Upload fully working

--- app/php/upload_data.php
+++ app/php/upload_data.php
@@ -79,67 +79,71 @@ if (move_uploaded_file($_FILES["audioname"]["tmp_name"], $target_file)) {
 
         $sql = "INSERT INTO Album (album_name, album_filename, artist_id) VALUES ('$album','test','$artist_id')";
         if ($conn->query($sql) === TRUE) {
-            echo "New record created successfully";
+            $album_id = mysqli_insert_id($conn);
         } else {
             dieWithError(18);
         }
-    } //shouldn't need an else here todo update album art
+    } else {
+        $album_id = mysqli_fetch_object($result)->album_id;
+    }
 
     /* Insert data */
-//        $sql = "INSERT INTO Data (filename,title, user_id, artist_id, album_id) VALUES ('$filename','$title','0','$artist_id','$album_id')";
-//
-//
-//        if ($result = mysqli_query($conn, $sql)) {
-//            $lastdtID = mysqli_insert_id($conn);
-//            echo 'Data id created: ' . $lastdtID . '<br>';
-//
-//            //Link each tag thats in the taglist (will be separated by commas)
-//            //todo make sure the front end validates that each tag in taglist is different or this will become a clusterfuck
-//            $tag_array = explode(',', $taglist);
-//            foreach ($tag_array as $tag) {
-//                $sql = "SELECT tag_id FROM Tag WHERE keyword = '$tag'";
-//                echo $tag . '<br>';
-//                $tgint = null;
-//                if ($result = mysqli_query($conn, $sql)) {
-//
-//                    //If tag does not exist insert it
-//                    if ($result->num_rows == 0) {
-//                        $sql = "INSERT INTO Tag (keyword) VALUES ('$tag')";
-//                        if ($result = mysqli_query($conn, $sql)) {
-//                            $lastID = mysqli_insert_id($conn);
-//                            echo 'New tag created: ' . $lastID . '<br>';
-//                            $tgint = $lastID;
-//                        }
-//                    } //the tag already exists
-//                    else {
-//                        $tgint = mysqli_fetch_object($result)->tag_id;
-//                    }
-//                } else {
-//                    dieWithError(17);
-//
-//                }
-//
-//                //insert a link between the tag and the data
-//                $sql = "INSERT INTO Data_tag (data_id, tag_id) VALUES ('$lastdtID','$tgint')";
-//                if ($result = mysqli_query($conn, $sql)) {
-//                    $lastID = mysqli_insert_id($conn);
-//                } else {
-//                    dieWithError(18);
-//
-//                }
-//            }
+    $sql = "INSERT INTO Data (filename,title, user_id, artist_id, album_id) VALUES ('$filename','$title','3','$artist_id','$album_id')";
+
+
+    if ($conn->query($sql) === TRUE) {
+        $lastdtID = mysqli_insert_id($conn);
+        echo 'Data id created: ' . $lastdtID . '<br>';
+
+        //Link each tag thats in the taglist (will be separated by commas)
+        //todo make sure the front end validates that each tag in taglist is different or this will become a clusterfuck
+        $tag_array = explode(',', $taglist);
+        foreach ($tag_array as $tag) {
+            $sql = "SELECT tag_id FROM Tag WHERE keyword = '$tag'";
+            $result = $conn->query($sql);
+            echo $tag . '<br>';
+            $tgint = null;
+            if ($result = mysqli_query($conn, $sql)) {
+
+                //If tag does not exist insert it
+                if ($result->num_rows == 0) {
+                    $sql = "INSERT INTO Tag (keyword) VALUES ('$tag')";
+                    if ($result = mysqli_query($conn, $sql)) {
+                        $lastID = mysqli_insert_id($conn);
+                        echo 'New tag created: ' . $lastID . '<br>';
+                        $tgint = $lastID;
+                    }
+                } //the tag already exists
+                else {
+                    $tgint = mysqli_fetch_object($result)->tag_id;
+                }
+            } else {
+                dieWithError(17);
+
+            }
+
+            //insert a link between the tag and the data
+            $sql = "INSERT INTO Data_tag (data_id, tag_id) VALUES ('$lastdtID','$tgint')";
+            if ($result = mysqli_query($conn, $sql)) {
+                $lastID = mysqli_insert_id($conn);
+            } else {
+                dieWithError(18);
+
+            }
+        }
     echo '<script>
                 alert("Upload successful!");
                 window.location.href="../html/library.php";
                 </script>';
 
-//        } else {
-//            dieWithError(12);
-//
-//        }
-//    } else {
-//        dieWithError(19);
+    } else {
+        echo 'Error: ', $conn->error;
 
-    //   }
+//            dieWithError(12);
+
+    }
+} else {
+    dieWithError(19);
+
 }
 ?>
